b43af6604908d274afb1a0c76a870be9786f0574
pete02 <petrik.rasanen@gmail.com>
2022-10-15 11:22:54 +0300

    1.2

--- tehtävät/osa 1/kurssitiedot/src/App.js
+++ tehtävät/osa 1/kurssitiedot/src/App.js
@@ -7,19 +7,19 @@ const Header=(props)=>{
     </div>
   )
 }
-
+const Part=(props)=>{
+  return(
+    <div>
+      <p>{props.part} {props.ex}</p>
+    </div>
+  )
+}
 const Content=(props)=>{
   return(
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} ex={props.exercises1}/>
+      <Part part={props.part2} ex={props.exercises2}/>
+      <Part part={props.part3} ex={props.exercises3}/>
     </div>
   )
 }
